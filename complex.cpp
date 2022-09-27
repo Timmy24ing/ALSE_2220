3a8f39b12d6a5695603022cec70f2db1bf35727e
alxperezECI <alexander.perez@escuelaing.edu.co>
2022-09-27 15:30:16 -0500

    Ajuste de indentaciones.

--- complex.cpp
+++ complex.cpp
@@ -2,68 +2,68 @@
 #include "complex.h"
 
 Complex::Complex(){
-		re = im = 0.;
-	}
+	re = im = 0.;
+}
 
-	Complex::Complex(double r, double i){
-		re = r;
-		im = i;
-	}
+Complex::Complex(double r, double i){
+	re = r;
+	im = i;
+}
 
-	double Complex::real(){
-		return re;
-	}
-	void   Complex::real(double r){
-	  re = r;
-	}
+double Complex::real(){
+	return re;
+}
+void   Complex::real(double r){
+  re = r;
+}
 
-	double Complex::imag(){
-	  return im;
-	}
+double Complex::imag(){
+  return im;
+}
 
-	void   Complex::imag(double i){
-	  im = i;
-	}
+void   Complex::imag(double i){
+  im = i;
+}
 
-	Complex Complex::operator+(const Complex &a){
-	  Complex c;
-	  c.re = re + a.re;
-	  c.im = im + a.im;
-	  return c;
-	}
+Complex Complex::operator+(const Complex &a){
+  Complex c;
+  c.re = re + a.re;
+  c.im = im + a.im;
+  return c;
+}
 
-	Complex Complex::operator-(const Complex &a){
-	  Complex c;
-	  c.re = re - a.re;
-	  c.im = im - a.im;
-	  return c;
-	}
+Complex Complex::operator-(const Complex &a){
+  Complex c;
+  c.re = re - a.re;
+  c.im = im - a.im;
+  return c;
+}
 
-	Complex Complex::operator*(const Complex &a){
-	  Complex c;
-	  c.re = re * a.re - im * a.im;
-	  c.im = re * a.im + im * a.re;
-	  return c;
-	}
+Complex Complex::operator*(const Complex &a){
+  Complex c;
+  c.re = re * a.re - im * a.im;
+  c.im = re * a.im + im * a.re;
+  return c;
+}
 
-	double Complex::magnitud(){
-		return sqrt( re * re + im * im );
-	}
+double Complex::magnitud(){
+	return sqrt( re * re + im * im );
+}
 
-	Complex Complex::conyugate(){
-		Complex c;
-		c.re = re;
-		c.im = -im;
-		return c;
-	}
+Complex Complex::conyugate(){
+	Complex c;
+	c.re = re;
+	c.im = -im;
+	return c;
+}
 
-	Complex Complex::inverse(){
-		Complex c;
-		c.re = - re;
-		c.im = -im;
-		return c;
-	}
+Complex Complex::inverse(){
+	Complex c;
+	c.re = - re;
+	c.im = -im;
+	return c;
+}
 
-	double Complex::angle(){
-		return atan2(im, re);
-	}
+double Complex::angle(){
+	return atan2(im, re);
+}
