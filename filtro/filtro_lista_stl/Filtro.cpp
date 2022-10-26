6070c8cb3b3453200e87bf0705c34d112db5c593
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2022-10-26 16:06:20 -0500

    datos de entrada.

--- filtro/filtro_lista_stl/Filtro.cpp
+++ filtro/filtro_lista_stl/Filtro.cpp
@@ -5,6 +5,7 @@
 
 #include "Filtro.h"
 #include <iostream>
+#include <list>
 
 /**
  * Filtro implementation
@@ -20,6 +21,10 @@ Filtro::Filtro(unsigned char tamano){
  */
 bool Filtro::agregarDato(float a) {
 	// Escrir el código para garantizar el tamaño fijo de la ventana de muestras
+
+    _memoria.push_back( a );
+    if( _memoria.size() > _tamano ) _memoria.pop_front();
+
     return true;
 }
 
@@ -27,10 +32,13 @@ bool Filtro::agregarDato(float a) {
  * @return float
  */
 float Filtro::promedio() {
-    float prom;
+    float prom = 0.;
     // Escribir el código para calcular el promedio de los datos de la lista
+    std::list<float>::iterator it;
+    for ( it = _memoria.begin(); it != _memoria.end(); ++it )
+        prom += *it;
 
-    return prom;
+    return prom / _memoria.size();
 }
 
 float& Filtro::salida_filtro(float &a){
